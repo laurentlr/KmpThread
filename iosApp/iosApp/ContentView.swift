f036ae3de979628195df1f5cf52e74db2ee5a2b6
Laurent <laurent.russier@gmail.com>
2024-05-21 17:41:02 +0200

    Add iOS 100 loop

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,24 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
-
-	var body: some View {
-		Text(greet)
-	}
+    
+    var body: some View {
+        Text(getGreetings())
+    }
+    
+   
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
 		ContentView()
 	}
+}
+
+func getGreetings()->String{
+    for _ in 0..<100 {
+        Greeting().greet()
+    }
+    return Greeting().greet()
 }
