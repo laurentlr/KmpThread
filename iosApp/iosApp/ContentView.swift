c75253256ab161437418bcb1de6b7998b3ed319c
Stanislaw Kabacinski @Ponzi <stanislaw.kabacinski@fairmoney.io>
2024-05-22 11:32:38 +0200

    add repro scenario

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -18,8 +18,13 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 func getGreetings()->String{
-    for _ in 0..<100 {
-        Greeting().greet()
+    for i in 0..<1000 {
+        DispatchQueue.global(qos: .background).async {
+            usleep(200000)
+            print("storage read before \(Thread.current)")
+            Greeting().greet()
+            print("storage read after \(Thread.current)")
+        }
     }
     return Greeting().greet()
 }
--- shared/src/commonMain/kotlin/laurent/russier/Greeting.kt
+++ shared/src/commonMain/kotlin/laurent/russier/Greeting.kt
@@ -1,20 +1,25 @@
 package laurent.russier
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
         runBlocking {
-            fakeDelay()
+            withContext(Dispatchers.Default) {
+                fakeDelay()
+            }
         }
         return "Hello, ${platform.name}!"
     }
 
     suspend fun fakeDelay() {
-        delay(100)
-        println("lolo delay 100")
+        delay(1000)
+        println("lolo delay 1000")
     }
 }
